e9caec3ae25c6858ae46b525ef38428886cf7d58
jrockb <jrockb@gmail.com>
2021-08-31 16:03:01 -0500

    Agregada clase pojo y metodo en el controlador como un ejemplo basico de MVC

--- spring-boot-web/src/main/java/co/com/jcd/springboot/app/web/app/controllers/IndexController.java
+++ spring-boot-web/src/main/java/co/com/jcd/springboot/app/web/app/controllers/IndexController.java
@@ -5,10 +5,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import co.com.jcd.springboot.app.web.app.models.Usuario;
+
 @Controller // clase controlador para manejar las peticiones del usuario desde el front
 @RequestMapping("/app") // ruta padre o de primer nivel, p.e. http://localhost:8080/app/index 
-public class IndexController {
-	
+public class IndexController {	
 	
 	@GetMapping({"/index","/","/home"}) 
 	public String index(Model model) { 
@@ -16,8 +17,16 @@ public class IndexController {
 		return "index";
 	}
 	
-	
-	
+	// ejemplo para enviar dato (modelo) desde el controlador a la vista
+	@RequestMapping("/perfil")
+	public String perfil(Model model) {
+		Usuario usuario = new Usuario();
+		usuario.setNombre("nombre");
+		usuario.setApellido("apellido");
+		model.addAttribute("usuario", usuario);
+		model.addAttribute("titulo","perfil del usuario: ".concat(usuario.getNombre()));
+		return "perfil";		
+	}
 	
 
 }

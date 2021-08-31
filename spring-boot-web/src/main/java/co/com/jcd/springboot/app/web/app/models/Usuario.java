d7c96a57e93b4812b2f1ddfac4ae159b7215df52
jrockb <jrockb@gmail.com>
2021-08-31 16:04:20 -0500

    pojo y vista nueva del ejemplo

---
+++ spring-boot-web/src/main/java/co/com/jcd/springboot/app/web/app/models/Usuario.java
@@ -0,0 +1,26 @@
+package co.com.jcd.springboot.app.web.app.models;
+
+// clase Pojo
+
+public class Usuario {
+	
+	private String nombre;
+	private String apellido;
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+	
+
+}

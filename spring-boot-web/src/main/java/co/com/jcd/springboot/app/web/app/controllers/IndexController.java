f9c3f80123e36321750db62ae835c1e24b1a376b
jrockb <jrockb@gmail.com>
2021-08-31 14:38:01 -0500

    agregada anotación RequestMapping para definir la ruta padre

--- spring-boot-web/src/main/java/co/com/jcd/springboot/app/web/app/controllers/IndexController.java
+++ spring-boot-web/src/main/java/co/com/jcd/springboot/app/web/app/controllers/IndexController.java
@@ -1,28 +1,22 @@
 package co.com.jcd.springboot.app.web.app.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller // clase controlador para manejar las peticiones del usuario desde el front
+@RequestMapping("/app") // ruta padre o de primer nivel, p.e. http://localhost:8080/app/index 
 public class IndexController {
 	
-	@GetMapping({"/index","/","/home"})
-	public ModelAndView index(ModelAndView mv) { // usando ModelAndView
-		mv.addObject("titulo", "Hola SpringFramework con ModelAndView!!");
-		mv.setViewName("index");
-		return mv; 
+	
+	@GetMapping({"/index","/","/home"}) 
+	public String index(Model model) { 
+		model.addAttribute("titulo","hola Spring Framework");
+		return "index";
 	}
 	
 	
-	/**
-	 * @GetMapping({"/index","/","/home"}) // el metodo está mapeado a estas tres rutas
-	public String index(ModelMap model) { // ModelMap es equivalente a Model
-		model.addAttribute("titulo","hola Spring Framework"); // titulo es el atributo
-		return "index"; // retorna el nombre de la vista
-	}
-	
-	 */
 	
 	
 

cd5d70ae23ac45544ac47746490f9e0523638a91
jrockb <jrockb@gmail.com>
2021-08-31 18:50:12 -0500

    Agregada vista de listar con un ejemplo de th:if

--- spring-boot-web/src/main/java/co/com/jcd/springboot/app/web/app/controllers/IndexController.java
+++ spring-boot-web/src/main/java/co/com/jcd/springboot/app/web/app/controllers/IndexController.java
@@ -1,5 +1,8 @@
 package co.com.jcd.springboot.app.web.app.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,6 +30,15 @@ public class IndexController {
 		model.addAttribute("usuario", usuario);
 		model.addAttribute("titulo","perfil del usuario: ".concat(usuario.getNombre()));
 		return "perfil";		
+	}
+	
+	// ejemplo con una lista de perfiles
+	@RequestMapping("/listar")
+	public String listar(Model model) {
+		List<Usuario> usuarios = new ArrayList<>();
+		model.addAttribute("titulo","Listado de usuarios");
+		model.addAttribute("usuarios", usuarios);
+		return "listar";		
 	}
 	
 
